3156867e61bbc2a2465115b98ba713c8365f1ad5
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-06-19 12:28:29 +0530

    Class1.cs

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using IndianStateCensusAnalyser.POCO;
+using NewtonSoft.Json;
+
+namespace IndianStateCensusAnalyser
+{
+    public class CensusAnalyser
+    {
+        public enum Country
+        {
+            INDIA, US, BRAZIL
+        }
+        Dictonary<string, CensusDTO> dataMap;
+
+        public Dictionary, CensusDTO> dataMap;
+
+public Dictionary<string, CensusDTO> LoadCensusData(Country country, string csvFilePath, string dataleaders)
+        {
+            dataMap = new(SVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeader);
+            return dataMap;
+        }
+    }
+}
