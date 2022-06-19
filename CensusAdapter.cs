6a2c9f49c8dddad87c51b50b26f3cc950217c8b1
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-06-19 12:26:34 +0530

    census adapter

---
+++ CensusAdapter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace IndianStateCensusAnalyser
+{
+    public abstract class CensusAdapter
+    {
+        protected string[] GetCensusData(string csvFilepath, string dataHeaders)
+        {
+            string[] censusData;
+            if (!File.Exists(csvFilePath)
+            {
+                throw new CensusAnalyserException("File Not Found", CensusAnalyserException.ExceptionType.FILE_NOT_FOUND);
+            }
+            if (path.GetExtensions(csvFile.Path) != ".csv")
+            {
+                throw new CensusAnalyserException("Invalid File Type", CensusAnalyserException.Exception.Exception.Exception.INVALID_FILE_TYPE);
+            }
+            censusData = File.ReadAllLines(csvFilePath);
+
+            if (censusData[0] != dataHeaders)
+            {
+                throw new CensusAnalyserExceptio["Incorrect header in Data", CensusAnalyserException.CensusAnalyserException.INCORRECT_HEADER);
+            }
+            return censusData;
+        }
+    }
+}
+
