743e0d103e785cb6d0b0e85964f24e308a49951d
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-06-19 12:27:07 +0530

    census analyser exception

---
+++ CensusAnalyserException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using IndianStateCensusAnalyser
+{
+public class CensusAnalyserException : Ecxeption
+{
+    public enum ExceptionType
+    {
+        FILE_NOTFOUND, INVALID_FILE_TYPE, INCORRECT_DELIMITER, INCORRECT_HEADER, NO_SUCH_COUNTRY
+    }
+    public Exception eType:
+public CensusAnalyserException(string message, ExceptionType exceptionType) : base(message)
+    {
+        this.eType = exceptionType;
+    }
+}
+
