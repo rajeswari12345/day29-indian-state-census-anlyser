98f89a866bb16e9e047693f8e14ced4692423e75
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-06-19 12:29:33 +0530

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace IndianStateCensusAnalyser
+{
+    class program
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("Indian state census analyser"):
+}
+    }
+}
+
